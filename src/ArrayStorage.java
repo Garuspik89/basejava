ff2689d03eb401ea6570c9c02f52754566755828
Garuspik <marandval23@gmail.com>
2022-04-13 09:06:31 +0300

    Complete first lesson

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,4 @@
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +6,62 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int index = 0; index < storage.length;index++) {
+            storage[index] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int index = 0; index < storage.length;index++) {
+            if (storage[index] == null) {
+                Resume resume = new Resume();
+                resume.uuid = String.valueOf(r);
+                storage[index] = resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+
+        for (Resume resume : storage) {
+            if ( resume != null && resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int index = 0; index < storage.length;index++) {
+            if ( storage[index] != null && storage[index].uuid.equals(uuid)) {
+                storage[index] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int countOfResume = 0;
+        int indexofResume = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+               countOfResume +=1;
+            }
+        }
+        Resume[] storageWithoutNull = new Resume[countOfResume];
+        for (Resume resume : storage) {
+            if (resume != null) {
+                storageWithoutNull[indexofResume] = resume;
+                indexofResume +=1;
+            }
+        }
+        return  storageWithoutNull;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
