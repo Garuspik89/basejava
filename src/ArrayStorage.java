373b8689b59fb03593cbff1c61167b3327250617
Garuspik <marandval23@gmail.com>
2022-04-26 16:37:41 +0300

    Complete first lesson

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,31 +1,26 @@
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        for (int index = 0; index < storage.length;index++) {
+        for (int index = 0; index < size; index++) {
             storage[index] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int index = 0; index < storage.length;index++) {
-            if (storage[index] == null) {
-                Resume resume = new Resume();
-                resume.uuid = String.valueOf(r);
-                storage[index] = resume;
-                break;
-            }
-        }
+        storage[size] = r;
+        size += 1;
     }
 
     Resume get(String uuid) {
 
         for (Resume resume : storage) {
-            if ( resume != null && resume.uuid.equals(uuid)) {
+            if (resume != null && resume.uuid.equals(uuid)) {
                 return resume;
             }
         }
@@ -33,9 +28,19 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int index = 0; index < storage.length;index++) {
-            if ( storage[index] != null && storage[index].uuid.equals(uuid)) {
+        int indexOfNull = -1;
+        for (int index = 0; index < size; index++) {
+            if (storage[index] != null && storage[index].uuid.equals(uuid)) {
                 storage[index] = null;
+                indexOfNull = index;
+                size -= 1;
+                break;
+            }
+        }
+        if (indexOfNull != -1) {
+            for (int index = indexOfNull; index < size; index++) {
+                storage[index] = storage[index + 1];
+                storage[index + 1] = null;
             }
         }
     }
@@ -44,30 +49,20 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int countOfResume = 0;
         int indexofResume = 0;
+        Resume[] storageWithoutNull = new Resume[size];
         for (Resume resume : storage) {
-            if (resume != null) {
-               countOfResume +=1;
-            }
-        }
-        Resume[] storageWithoutNull = new Resume[countOfResume];
-        for (Resume resume : storage) {
-            if (resume != null) {
+            if (resume == null) {
+                break;
+            } else {
                 storageWithoutNull[indexofResume] = resume;
-                indexofResume +=1;
+                indexofResume += 1;
             }
         }
-        return  storageWithoutNull;
+        return storageWithoutNull;
     }
 
     int size() {
-        int countOfResume = 0;
-        for (Resume resume : storage) {
-            if ( resume != null) {
-                countOfResume += 1;
-            }
-        }
-        return  countOfResume;
+        return size;
     }
 }
