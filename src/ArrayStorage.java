f315da73a2a94bee8d9ce173b9dc0365d8be1d1c
Garuspik <marandval23@gmail.com>
2022-05-05 17:10:16 +0300

    Complete first lesson

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,22 +6,21 @@ public class ArrayStorage {
     int size = 0;
 
     void clear() {
-        for (int index = 0; index < size; index++) {
-            storage[index] = null;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
         size = 0;
     }
 
     void save(Resume r) {
         storage[size] = r;
-        size += 1;
+        size++;
     }
 
     Resume get(String uuid) {
-
-        for (Resume resume : storage) {
-            if (resume != null && resume.uuid.equals(uuid)) {
-                return resume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
@@ -29,19 +28,19 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int indexOfNull = -1;
-        for (int index = 0; index < size; index++) {
-            if (storage[index] != null && storage[index].uuid.equals(uuid)) {
-                storage[index] = null;
-                indexOfNull = index;
-                size -= 1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                indexOfNull = i;
                 break;
             }
         }
         if (indexOfNull != -1) {
-            for (int index = indexOfNull; index < size; index++) {
-                storage[index] = storage[index + 1];
-                storage[index + 1] = null;
+            for (int i = indexOfNull; i < size; i++) {
+                storage[i] = storage[i + 1];
+                indexOfNull += 1;
             }
+            size--;
+            storage[indexOfNull] = null;
         }
     }
 
@@ -49,15 +48,9 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int indexofResume = 0;
         Resume[] storageWithoutNull = new Resume[size];
-        for (Resume resume : storage) {
-            if (resume == null) {
-                break;
-            } else {
-                storageWithoutNull[indexofResume] = resume;
-                indexofResume += 1;
-            }
+        for (int i = 0; i < size; i++) {
+            storageWithoutNull[i] = storage[i];
         }
         return storageWithoutNull;
     }
